cd1f0a6a2269ca3728bb9c22d03eb829c1887a86
nasri samy <nasri.samy.dev@gmail.com>
2019-12-31 14:27:39 +0100

    add def show

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,6 +2,10 @@ class ArticleController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
  def create
   Article.create title: params[:title]
   redirect_to "/article"
