83435aa890ecb4dcf295686bbab3db71596103c8
nasri samy <nasri.samy.dev@gmail.com>
2019-12-31 14:10:59 +0100

    add function create in controller

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,4 +2,8 @@ class ArticleController < ApplicationController
   def index
     @articles = Article.all
   end
+ def create
+  Article.create title: params[:title]
+  redirect_to "/article"
+ end
 end
