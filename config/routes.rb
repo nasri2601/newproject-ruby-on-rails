ffd951cbce16895d474bc6756c67b53bb783c884
nasri samy <nasri.samy.dev@gmail.com>
2020-01-03 17:53:40 +0100

    :fire: add route to destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'article'=>'article#index'
   get 'article/:id'=>'article#show'
   patch 'article/:id'=>'article#update'
+  delete 'article/:id'=>'article#destroy'
   post 'article'=>'article#create'
 
   root 'pages#home'
