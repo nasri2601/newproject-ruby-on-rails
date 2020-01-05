45053ea5d43371a46a8c4d0b252b14b21bc80e10
nasri samy <nasri.samy.dev@gmail.com>
2020-01-05 18:21:47 +0100

    :zap:

---
+++ db/migrate/20200105164902_add_category_id_to_articles.rb
@@ -0,0 +1,8 @@
+class AddCategoryIdToArticles < ActiveRecord::Migration[5.0]
+  def change
+    add_column :articles, :category_id, :integer
+    add_column :articles, :content, :string
+    add_index :articles, :category_id
+
+  end
+end
