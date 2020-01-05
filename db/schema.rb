44860611b078a7da56d2902d705b75014e0b8733
nasri samy <nasri.samy.dev@gmail.com>
2020-01-05 17:40:49 +0100

    :fire:

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191230121102) do
+ActiveRecord::Schema.define(version: 20200105163621) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
 end
